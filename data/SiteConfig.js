f5ca5bc524b0825ea6635f26df8ec4488f188203
Eddie Lee <eddielee6@me.com>
2018-05-11 20:57:32 +0100

    Update alt manifest theme

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -24,7 +24,7 @@ module.exports = {
   copyright: {
     label: "Eddie Lee"
   },
-  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  themeColor: "#ba3a34", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff", // Used for setting manifest background color.
   promoteGatsby: false
 };
